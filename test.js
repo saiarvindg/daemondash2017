0cc825553b6d16d0dbc52b569c975919956662b1
Daniel Choi <danielchoi42@gmail.com>
2017-01-28 18:37:02 -0500

    Fixed foreach loops
    fixed foreach loops

--- test.js
+++ test.js
@@ -116,7 +116,7 @@ validateFirst = function(classes) {
     for (count = 0; count < classes.length; count++) {
         var newGoodOnes = [[]];
         //Go through all the section times per class
-        for each (var meeting in c.times) {
+        forEach(var meeting in c.times) {
             var foundMatch = false;
             //MeetingTimes in the class
             if (count == 0) { //add array with the single meeting to goodOnes
@@ -125,7 +125,7 @@ validateFirst = function(classes) {
                 goodOnes.push(tmp);
             } else {
                 //Current  set of already compatible section times in goodOnes 
-                for each (var possibleMeetings in goodOnes) {
+                forEach(var possibleMeetings in goodOnes) {
                     //no conflicts with this possibleMeeting combination
                     if (!hasConflict(meeting, possibleMeetings)) {
                        newGoodOnes.push(possibleMeetings.push(meeting)); 
@@ -148,9 +148,9 @@ validateFirst = function(classes) {
 
 //Determines if a meeting conflicts with a set of already validated meetings
 hasConflict = function(curr, sections) {
-    for each (var t in curr.times) { // times is a field in the meetingTimes object.
-        for each (var s in sections) {
-            for each (var st in s.times) {
+    forEach(var t in curr.times) { // times is a field in the meetingTimes object.
+        forEach(var s in sections) {
+            forEach(var st in s.times) {
                 if (t.day == st.day) {
                     if ((t.start > st.start && t.start < st.end) || (t.end > st.start && t.end < st.end)) {
                         return true;
