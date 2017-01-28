0bbf71bacb086ab57aca735f8e338d862b27d3b6
Christian <christiannguyendinh@yahoo.com>
2017-01-28 15:12:27 -0500

    loading data stuff

--- test.js
+++ test.js
@@ -1,32 +1,71 @@
 var unirest = require('unirest');
 var moment = require('moment');
 
-var sections = [];
+/*
 
-unirest
-.get('http://api.umd.io/v0/courses/ENES100')
-.end((response) => {
-    sections = response.body['sections'];
-    //console.log(response.body['sections']);
+{
+    className: [
+        {
+            section: fdsaf
+            times: [
+                {
+                    days: MWF,
+                    start: fdsa,
+                    end: fewage
+                },
+            ]
+        },
+        ...
+    ],
+    ...
+}
 
-    sections.map((section) => {
-        unirest
-        .get('http://api.umd.io/v0/courses/sections/' + section)
-        .end((response) => {
-            //console.log(response.body);
-            console.log(response.body['section_id']);
-            console.log(response.body['meetings'][0]['days']);
-            //console.log(response.body['meetings'][0]['start_time']);
-            //console.log(response.body['meetings'][0]['end_time']);
-            var startTime = response.body['meetings'][0]['start_time'];
-            var endTime = response.body['meetings'][0]['end_time']
-            console.log("unformatted start time: " + startTime);
-            console.log("unformatted end time: " + endTime);
-            var startTimeFormatted = moment(startTime, ["h:mmA"]).format("HHmm");
-            var endTimeFormatted = moment(endTime, ["h:mmA"]).format("HHmm");
-            console.log("formatted start time: " + startTimeFormatted);
-            console.log("formatted end time: " + endTimeFormatted);
+*/
+testFunction = () => {
+
+    var sections = [];
+    var classes = {};
+
+    unirest
+    .get('http://api.umd.io/v0/courses/ENES100')
+    .end((response) => {
+        sections = response.body['sections'];
+        //console.log(response.body['sections']);
+        var className = response.body['course_id'];
+        classes[className] = [];
+        sections.map((section) => {
+            unirest
+            .get('http://api.umd.io/v0/courses/sections/' + section)
+            .end((response) => {
+                //console.log(response.body);
+                console.log(response.body['section_id']);
+                //console.log(response.body['meetings'][0]['start_time']);
+                //console.log(response.body['meetings'][0]['end_time']);
+
+                var sectionName = response.body['section_id'];
+                var meetingTimes = [];
+                response.body['meetings'].map((meeting) => {
+                    var startTime = meeting['start_time'];
+                    var endTime = meeting['end_time']
+                    var startTimeFormatted = moment(startTime, ["h:mmA"]).format("HHmm");
+                    var endTimeFormatted = moment(endTime, ["h:mmA"]).format("HHmm");
+                    console.log(response.body['meetings'][0]['days']);
+
+                    meetingTimes.push({
+                        days: response.body['meetings'][0]['days'],
+                        start: startTimeFormatted,
+                        end: endTimeFormatted
+                    });
+                });
+
+                classes[className].push({
+                    section: sectionName,
+                    times: meetingTimes
+                });
+            });
         });
+        console.log(classes);
     });
-});
+}
 
+module.exports = testFunction;
