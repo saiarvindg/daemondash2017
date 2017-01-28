3bf575cf7437edc6fce677ef600b5b3462e10f83
Christian <christiannguyendinh@yahoo.com>
2017-01-28 18:07:51 -0500

    load multiple classes

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var moment = require('moment');
 
 var sections = [];
 var classes = {};
-var flag = 1;
+var innerFlag = 0;
 var please = "11";
 
 /*
@@ -28,72 +28,76 @@ var please = "11";
 
 */
 
-testFunction = function() {
-    unirest
-    .get('http://api.umd.io/v0/courses/ENES100')
-    .end((response) => {
-        sections = response.body['sections'];
-        //console.log(response.body['sections']);
-        var className = response.body['course_id'];
-        flag = sections.length;
-        classes[className] = [];
-        sections.map(function(section) {
-            unirest
-            .get('http://api.umd.io/v0/courses/sections/' + section)
-            .end(function(response) {
-                //console.log(response.body);
-                //console.log(response.body['section_id']);
-                //console.log(response.body['meetings'][0]['start_time']);
-                //console.log(response.body['meetings'][0]['end_time']);
 
-                var sectionName = response.body['section_id'];
-                var meetingTimes = [];
-                response.body['meetings'].map((meeting) => {
-                    var startTime = meeting['start_time'];
-                    if (startTime == "")
-                        return;
-                    
-                    var endTime = meeting['end_time']
-                    var startTimeFormatted = moment(startTime, ["h:mmA"]).format("HHmm");
-                    var endTimeFormatted = moment(endTime, ["h:mmA"]).format("HHmm");
-                    //console.log(response.body['meetings'][0]['days']);
 
-                    var dayString = response.body['meetings'][0]['days'].toString();
+testFunction = function(classNames) {
+    for (let k = 0; k < classNames.length; k++) {
+        unirest
+        .get('http://api.umd.io/v0/courses/' + classNames[k])
+        .end((response) => {
+            sections = response.body['sections'];
+            //console.log(response.body['sections']);
+            var className = response.body['course_id'];
+            innerFlag += sections.length;
+            classes[className] = [];
+            sections.map(function(section) {
+                unirest
+                .get('http://api.umd.io/v0/courses/sections/' + section)
+                .end(function(response) {
+                    //console.log(response.body);
+                    //console.log(response.body['section_id']);
+                    //console.log(response.body['meetings'][0]['start_time']);
+                    //console.log(response.body['meetings'][0]['end_time']);
 
-                    for (let i = 0; i < dayString.length - 1; i++) {
-                        var dayChar = "";
-                        if (dayString[i] != 'T') {
-                            dayChar = dayString[i];
-                        } else {
-                            dayChar = dayString[i] + dayString[i + 1];
-                            i++;
-                        }
+                    var sectionName = response.body['section_id'];
+                    var meetingTimes = [];
+                    response.body['meetings'].map((meeting) => {
+                        var startTime = meeting['start_time'];
+                        if (startTime == "")
+                            return;
                         
-                        var sectionObj = {
-                            days: dayChar,
-                            start: startTimeFormatted,
-                            end: endTimeFormatted
+                        var endTime = meeting['end_time']
+                        var startTimeFormatted = moment(startTime, ["h:mmA"]).format("HHmm");
+                        var endTimeFormatted = moment(endTime, ["h:mmA"]).format("HHmm");
+                        //console.log(response.body['meetings'][0]['days']);
+
+                        var dayString = response.body['meetings'][0]['days'].toString();
+
+                        for (let i = 0; i < dayString.length - 1; i++) {
+                            var dayChar = "";
+                            if (dayString[i] != 'T') {
+                                dayChar = dayString[i];
+                            } else {
+                                dayChar = dayString[i] + dayString[i + 1];
+                                i++;
+                            }
+                            
+                            var sectionObj = {
+                                days: dayChar,
+                                start: startTimeFormatted,
+                                end: endTimeFormatted
+                            }
+                            //console.log(sectionObj);
+                            
+                            meetingTimes.push(sectionObj);
                         }
-                        //console.log(sectionObj);
-                        
-                        meetingTimes.push(sectionObj);
+                    });
+
+                    classes[className].push({
+                        section: sectionName,
+                        times: meetingTimes
+                    });
+
+                    innerFlag--;
+                    if (innerFlag == 0) {
+                        //console.log(classes)
+                        asdf(classes);
+                        return classes;
                     }
                 });
-
-                classes[className].push({
-                    section: sectionName,
-                    times: meetingTimes
-                });
-
-                flag--;
-                if (flag == 0) {
-                    //console.log(classes)
-                    asdf(classes);
-                    return classes;
-                }
             });
         });
-    });
+    }
 };
 
 /*
@@ -106,4 +110,4 @@ asdf = function(mess) {
 
 }
 
-testFunction();
+testFunction(['ENES100', 'CMSC131']);
