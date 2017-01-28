5096ec05fe1d94e7f052a1f0d5f2141b50cb38e0
Sai Arvind <sai.arvindg@gmail.com>
2017-01-28 14:20:22 -0500

    added 12 hour to 24 hour time conversion

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var unirest = require('unirest');
+var moment = require('moment');
 
 var sections = [];
 
@@ -15,8 +16,17 @@ unirest
             //console.log(response.body);
             console.log(response.body['section_id']);
             console.log(response.body['meetings'][0]['days']);
-            console.log(response.body['meetings'][0]['start_time']);
-            console.log(response.body['meetings'][0]['end_time']);
+            //console.log(response.body['meetings'][0]['start_time']);
+            //console.log(response.body['meetings'][0]['end_time']);
+            var startTime = response.body['meetings'][0]['start_time'];
+            var endTime = response.body['meetings'][0]['end_time']
+            console.log("unformatted start time: " + startTime);
+            console.log("unformatted end time: " + endTime);
+            var startTimeFormatted = moment(startTime, ["h:mmA"]).format("HHmm");
+            var endTimeFormatted = moment(endTime, ["h:mmA"]).format("HHmm");
+            console.log("formatted start time: " + startTimeFormatted);
+            console.log("formatted end time: " + endTimeFormatted);
         });
     });
 });
+
