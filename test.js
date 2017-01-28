e12318d288f6681c25cc3b54b521c7604c65d355
Daniel Choi <danielchoi42@gmail.com>
2017-01-28 17:58:41 -0500

    Added validateFirst
    Added Tim's function validateFirst

--- test.js
+++ test.js
@@ -101,12 +101,48 @@ while (flag != 0) {
     console.log(flag);
 }
 console.log(classes);*/
-asdf = function(mess) {
-    console.log(mess);
 
+// Contains functions to help filter one person
+validateFirst = function(classes) {
+    var goodOnes = [[]];
+    var notPossible = false;
+    //contains sets of valid MeetingTimes
+    var count;
+    //Go through all classes
+    for (count = 0; count < classes.length; count++) {
+        var newGoodOnes = [[]];
+        //Go through all the section times per class
+        for each (var meeting in c.times) {
+            var foundMatch = false;
+            //MeetingTimes in the class
+            if (count == 0) { //add array with the single meeting to goodOnes
+                var tmp = [];
+                tmp.push(meeting);
+                goodOnes.push(tmp);
+            } else {
+                //Current  set of already compatible section times in goodOnes 
+                for each (var possibleMeetings in goodOnes) {
+                    //no conflicts with this possibleMeeting combination
+                    if (!hasConflict(meeting, possibleMeetings)) {
+                       newGoodOnes.push(possibleMeetings.push(meeting)); 
+                       foundMatch = true;
+                    }
+                }
+            }
+            
+            if (!foundMatch) {
+                return null;
+            }        
+        }
+        
+        goodOnes = newGoodOnes;
+    }
+    
+    return goodOnes;
 }
 
-//helper for asdf
+
+//Determines if a meeting conflicts with a set of already validated meetings
 hasConflict = function(curr, sections) {
     for each (var t in curr.times) { // times is a field in the meetingTimes object.
         for each (var s in sections) {
